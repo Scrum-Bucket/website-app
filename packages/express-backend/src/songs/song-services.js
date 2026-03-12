36a2dc7b6c560d3ac02200914edce6def5eb35a7
Gabriel <gabrielrocha101@icloud.com>
2026-03-11 22:07:03 -0700

    Add song functions backend

---
+++ packages/express-backend/src/songs/song-services.js
@@ -0,0 +1,43 @@
+//song-services.js
+import mongoose from "mongoose";
+import song from "./song.js";
+
+mongoose.set("debug", true);
+
+mongoose
+  // change to atlas DB link later
+  .connect("mongodb://localhost:27017/songs")
+  .catch((error) => console.log(error));
+
+
+function getSongs(songLink) {
+  if (!songLink) return song.find();
+  return song.find({ songLink });
+}
+
+function findSongById(id) {
+  return song.findById(id);
+}
+
+function addSong(songLink, details = []) {
+  const newSong = new song({ songLink, details });
+  return newSong.save();
+}
+
+function deleteSong(id) {
+  return song.findByIdAndDelete(id);
+}
+
+// searchSong two search by a keyword in details array
+function searchSong(keyword) {
+  if (!keyword) return song.find();
+  return song.find({ details: { $elemMatch: { $regex: keyword, $options: "i" } } });
+}
+
+export default {
+  getSongs,
+  findSongById,
+  addSong,
+  deleteSong,
+  searchSong,
+};
