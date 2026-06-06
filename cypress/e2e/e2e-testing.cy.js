f765cbdce652c1e3360db492c7ab62ba42421e09
LupawnSansei <wedembois666@gmail.com>
2026-06-05 22:24:34 -0700

    one more each

--- cypress/e2e/e2e-testing.cy.js
+++ cypress/e2e/e2e-testing.cy.js
@@ -6,6 +6,11 @@
  *    WHEN I continue as a guest
  *    THEN I arrive on the home page
  *    AND I can see the room navigation options
+ *
+ * Scenario: Guest opens the room-code join page
+ *    GIVEN I am on the home page as a guest
+ *    WHEN I choose Join Room
+ *    THEN I arrive on the join-code page
  */
 
 describe("Guest app entry", () => {
@@ -17,5 +22,16 @@ describe("Guest app entry", () => {
     cy.location("pathname").should("eq", "/home");
     cy.contains("Welcome, Guest").should("be.visible");
     cy.contains("button", "Join Room").should("be.visible");
+  });
+
+  it("lets a guest navigate to the join-code page", () => {
+    cy.visit("/");
+    cy.contains("button", "Continue as Guest").click();
+
+    cy.contains("button", "Join Room").click();
+
+    cy.location("pathname").should("eq", "/home/code");
+    cy.contains("Join by Code").should("be.visible");
+    cy.get('input[aria-label="Room code"]').should("be.visible");
   });
 });
